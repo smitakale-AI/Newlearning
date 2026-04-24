7e47b0353da627217b3fea7d7eea411097a403c5
smitakale-AI <smitakale1528@gmail.com>
2026-04-24 09:52:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,Y,test size=0.2,random_state=42
 
 #Train the model
 
-model=LinearRegression )
+model=LinearRegression()
 model.fit(X_train,y_train)
 
 #Streamlit User Interface
